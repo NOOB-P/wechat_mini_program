01ec3ea651023f13f1836e3438b19ba897be57ce
miegoist <3158575045@qq.com>
2026-04-23 21:00:53 +0800

    合并修改1

--- JavaSB_back/src/main/java/com/edu/javasb_back/repository/SysNotificationRepository.java
+++ JavaSB_back/src/main/java/com/edu/javasb_back/repository/SysNotificationRepository.java
@@ -1,11 +1,12 @@
 package com.edu.javasb_back.repository;
 
-import com.edu.javasb_back.model.entity.SysNotification;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.edu.javasb_back.model.entity.SysNotification;
 
 @Repository
 public interface SysNotificationRepository extends JpaRepository<SysNotification, Long> {
